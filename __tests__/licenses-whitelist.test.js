22c6a44ef176fc7114e2e69d7ac1e28b551d78c9
Iran Reyes <iran.reyes@jam3.com>
2019-06-02 03:27:44 -0400

    test: removing testing code

--- __tests__/licenses-whitelist.test.js
+++ __tests__/licenses-whitelist.test.js
@@ -1,13 +1,7 @@
 'use strict';
 
-const LicenseWhitelist = require('../src/rules/licenses-whitelist');
-
-const licensewhitelist = new LicenseWhitelist();
-
-licensewhitelist.run();
-
-// describe('Min Vulnerabilities rule', () => {
-//   test('sample test', () => {
-//     expect(1).toBe(1);
-//   });
-// });
+describe('License Whitelist', () => {
+  test('sample test', () => {
+    expect(1).toBe(1);
+  });
+});
