52c5103be9d82e127d5f16aab96ee1cfe6ed604f
Iran Reyes <iran.reyes@jam3.com>
2019-08-23 00:07:06 -0400

    feat: migrate plugin index to new plugin structure

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@
 const path = require('path');
 const requireIndex = require('requireindex');
 
-const plugin = {
-  rules: requireIndex(path.join(__dirname, '/rules'))
+const Adviser = require('adviser');
+
+class Dependencies extends Adviser.Plugin {
+  constructor(settings) {
+    super(settings);
+
+    this.rules = requireIndex(path.join(__dirname, '/rules'));
+  }
+}
+
+Dependencies.meta = {
+  description: 'Rules related to package.json dependencies',
+  recommended: true
 };
 
-module.exports = plugin;
+module.exports = Dependencies;
