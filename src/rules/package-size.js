72a285da5fd50456ad2c2dce30f9d55c666e408d
Colin MacLachlan <cmaclachlan@fb.com>
2019-09-04 12:50:58 -0400

    fix: acquire version from module package.json, resolve package name discrepancy bug

--- src/rules/package-size.js
+++ src/rules/package-size.js
@@ -29,7 +29,7 @@ class PackageSize extends Adviser.Rule {
 
   _generateVerboseReport(packages = [], skips = []) {
     const inlinePackages = packages.reduce((accu, pkg) => {
-      return ` ${accu}   - ${pkg.name}: ${pkg.gzip} kb \n`;
+      return ` ${accu}   - ${pkg.name} ${pkg.version}: ${pkg.gzip} kb \n`;
     }, '\n');
 
     const baseMessage = `Found heavy packages: ${inlinePackages}`;
@@ -47,52 +47,52 @@ class PackageSize extends Adviser.Rule {
 
   async run(sandbox) {
     const packagejson = require(path.join(this.context.filesystem.dirname, 'package.json'));
-    const packages = [];
+    const processing = { packages: [], stats: [] };
     const promises = [];
-    const results = [];
-    const skip = [];
+    const results = { complete: [], skip: [] };
 
     if (packagejson.hasOwnProperty('dependencies')) {
       const dependencies = Object.keys(packagejson['dependencies']);
-      const versionFilter = new RegExp('[^0-9.]+');
 
-      try {
-        // attempting to identify the most accurate version number possible using package-lock.json
-        const packagelockjson = require(path.join(this.context.filesystem.dirname, 'package-lock.json'));
+      dependencies.forEach((name, index) => {
+        if (!this.parsedOptions.whitelist.includes(name)) {
+          const dependency = { name: name, version: '' };
 
-        dependencies.forEach((name, index) => {
-          const version = packagelockjson['dependencies'][name]['version'].replace(versionFilter, '');
-          packages.push({ name: name, version: version });
-        });
-      } catch {
-        dependencies.forEach((name, index) => {
-          const version = packagejson['dependencies'][name].replace(versionFilter, '');
-          packages.push({ name: name, version: version });
-        });
-      }
+          try {
+            dependency.version = require(`${this.context.filesystem.dirname}/node_modules/${
+              dependency.name
+            }/package.json`).version;
+          } catch {
+            dependency.version = 'latest';
+          }
+
+          processing.packages.push(dependency);
+          promises.push(
+            getBuiltPackageStats(`${dependency.name}@${dependency.version}`, { client: 'npm' }).catch(() => null)
+          );
+        }
+      });
     }
 
-    packages.forEach(pkg => {
-      if (!this.parsedOptions.whitelist.includes(pkg.name)) {
-        promises.push(getBuiltPackageStats(`${pkg.name}@${pkg.version}`, { client: 'npm' }).catch(() => null));
-      }
-    });
+    processing.stats = await Promise.all(promises);
 
-    const values = await Promise.all(promises);
-
-    values.forEach((value, index) => {
+    processing.stats.forEach((value, index) => {
       if (!value) {
-        skip.push({ name: packages[index].name });
+        results.skip.push({ name: processing.packages[index].name });
       }
 
       if (value && value.gzip >= this.parsedOptions.threshold * 1000) {
-        results.push({ name: packages[index].name, gzip: (value.gzip / 1000).toFixed(2) });
+        results.complete.push({
+          name: processing.packages[index].name,
+          version: processing.packages[index].version,
+          gzip: (value.gzip / 1000).toFixed(2)
+        });
       }
     });
 
-    if (results.length > 0) {
-      const message = this._generateReport(results);
-      const verbose = this._generateVerboseReport(results, skip);
+    if (results.complete.length > 0) {
+      const message = this._generateReport(results.complete);
+      const verbose = this._generateVerboseReport(results.complete, results.skip);
 
       const report = {
         message,
