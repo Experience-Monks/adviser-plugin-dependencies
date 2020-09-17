541c9871b160a0eff9182abbbfff80cffb409a24
tyrellbain <contact@tyrellbain.com>
2020-09-17 17:13:07 -0400

    Remove unneeded logic

--- src/rules/suspicious-packages.js
+++ src/rules/suspicious-packages.js
@@ -6,7 +6,6 @@ const request = require('request-promise-native');
 const Adviser = require('adviser');
 
 const DEPENDENCIES = ['dependencies', 'devDependencies', 'peerDependencies'];
-// const INDICATORS = ['downloads', 'last-update', 'maintainers', 'open-issues', 'stars', 'watchers', 'forks', 'archived'];
 const INDICATORS = Object.freeze({
   DOWNLOADS: 'downloads',
   LAST_UPDATE: 'last-update',
@@ -16,34 +15,21 @@ const INDICATORS = Object.freeze({
   WATCHERS: 'watchers',
   FORKS: 'forks'
 });
-const RUN_LOG = '.suspicious-packages-log';
 
 class SuspiciousPackage extends Adviser.Rule {
   constructor(context) {
     super(context);
 
-    if (!this.context.options.indicators) {
+    if (
+      this.context.options.indicators.length < 1 ||
+      !this.context.options.indicators ||
+      Array.isArray(this.context.options.indicators)
+    ) {
       throw new Error(`Wrong indicators, an array with indicators is expected`);
     }
     this.packageData = this._genPackageData();
     this.parsedOptions = { ...this.context.options };
   }
-
-  /* 
-  IF log file exists
-    Get package.json change time
-    Get Log file lastRun prop
-    IF change time is newer than last run
-      Run through checks
-    ELSE
-      Output message to say we aren't running
-  */
-
-  // Checks:
-  // see which indicators are within the options
-  // query for those using functions
-  // CREATE OBJ: indicators which don't meet threshold
-  // Return message
 
   async run(sandbox) {
     var lastRuntime = await this._genRuntime();
@@ -51,31 +37,45 @@ class SuspiciousPackage extends Adviser.Rule {
       lastRuntime = Date.now();
     }
 
-    const modifiedTime = await this._genPackageChangeTime();
+    const indicators = this.parsedOptions.indicators;
 
-    // IF change time is newer than last run
-    if (Number.parseFloat(modifiedTime) - Number.parseFloat(lastRuntime) > 0) {
-      const packagejson = require(path.join(this.context.filesystem.dirname, 'package.json'));
-
-      // Checks:
-      // see which indicators are within the options
-      // query for those using functions
-      // CREATE OBJ: indicators which don't meet threshold
-      // Return message
-
-      let packages = [];
-      DEPENDENCIES.map(dep => {
-        if (packagejson[dep] !== undefined) {
-          packages = packages.concat(Object.keys(packagejson[dep]));
+    let packages = await this._getValidPackages().map(pkg => {
+      const packageData = { packageName: pkg };
+      console.log(indicators);
+      indicators.keys(indicator => {
+        console.log(this.packageData);
+        switch (indicator) {
+          case INDICATORS.STARS:
+            this.packageData[INDICATORS.STARS] = this._genPackageStars(pkg);
+            break;
         }
       });
+      return packageData;
+    });
+    console.log(packages);
 
-      // selected indicators to look for on packages
-      // can use dot notation to scry through
-      let indicators = this.parsedOptions.indicators;
+    // selected indicators to look for on packages
+    // can use dot notation to scry through
+    // let indicators = this.parsedOptions.indicators;
 
-      this._setRuntime();
-    }
+    // console.log(await this._genDownloadCount(packages[0]));
+    // console.log(await this._genPackageLastUpdated(packages[0]));
+    // console.log(await this._genPackageMaintainers(packages[0]));
+    // console.log(await this._genPackageOpenIssues(packages[0]));
+    // console.log(await this._genPackageStars(packages[0]));
+    // console.log(await this._genPackageForks(packages[0]));
+
+    this._setRuntime();
+    // }
+  }
+
+  _getValidPackages() {
+    const packagejson = require(path.join(this.context.filesystem.dirname, 'package.json'));
+    return DEPENDENCIES.flatMap(dep => {
+      if (packagejson[dep] !== undefined) {
+        return Object.keys(packagejson[dep]);
+      }
+    }).filter(pkg => pkg);
   }
 
   _genPackageChangeTime() {
@@ -103,8 +103,6 @@ class SuspiciousPackage extends Adviser.Rule {
       return packageCache[packageName];
     };
   }
-
-  //         //['downloads', 'last-update', 'maintainers', 'open-issues', 'stars', 'watchers', 'forks', 'archived']
 
   _genDownloadCount(packageName = '') {
     if (packageName) {
@@ -163,36 +161,6 @@ class SuspiciousPackage extends Adviser.Rule {
       return github.forksCount;
     }
     throw new Error(`A valid package name is expected`);
-  }
-
-  _setRuntime() {
-    fs.open(path.join(this.context.filesystem.dirname, RUN_LOG), 'w', (err, fd) => {
-      if (!err) {
-        fs.write(fd, JSON.stringify({ lastRun: Date.now() }), err => {
-          if (err) throw err;
-        });
-      }
-      fs.close(fd);
-    });
-  }
-
-  _genRuntime() {
-    return new Promise((resolve, reject) =>
-      fs.stat(path.join(this.context.filesystem.dirname, RUN_LOG), (err, stats) => {
-        if (err) return reject(err);
-        fs.open(path.join(this.context.filesystem.dirname, RUN_LOG), 'r', (err, fd) => {
-          if (!err) {
-            let bufferSize = Number.parseInt(stats.size) || 30;
-            fs.read(fd, Buffer.alloc(Number.parseInt(bufferSize)), 0, bufferSize, 0, (err, read, buffer) => {
-              let jsonStringFromLog = buffer.toString().slice(0, read);
-              fs.close(fd);
-              if (err) return reject(err);
-              resolve(JSON.parse(jsonStringFromLog).lastRun);
-            });
-          }
-        });
-      })
-    );
   }
 }
 
